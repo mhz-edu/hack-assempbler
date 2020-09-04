35aff7ea1a9222be5af218f71571040f18fb6b3f
unknown <mhz.edu@gmail.com>
2020-09-04 23:50:50 +0300

    Add initial symbols support

--- hack_asm.py
+++ hack_asm.py
@@ -2,6 +2,33 @@ import argparse
 import re
 import os
 
+SYMBOL_TABLE = {
+    'R0':     0,
+    'R1':     1,
+    'R2':     2,
+    'R3':     3,
+    'R4':     4,
+    'R5':     5,
+    'R6':     6,
+    'R7':     7,
+    'R8':     8,
+    'R9':     9,
+    'R10':    10,
+    'R11':    11,
+    'R12':    12,
+    'R13':    13,
+    'R14':    14,
+    'R15':    15,
+    'SCREEN': 16384,
+    'KBD':    24576,
+    'SP':     0,
+    'LCL':    1,
+    'ARG':    2,
+    'THIS':   3,
+    'THAT':   4,
+    'INS_PTR': 16
+}
+
 JUMP_TABLE = {
     '':    '000',
     'JGT': '001',
@@ -62,32 +89,55 @@ def writeFile(data, filename):
 
 
 def parseA(line):
-    m = re.match(r'^\s*@(\d+)', line)
+    m = re.match(r'^\s*@([^\s|//.*]+)', line)
     if m:
-        return '0{:0=15b}\n'.format(int(m.group(1)))
+        if m.group(1).isnumeric():
+            return '0{:0=15b}\n'.format(int(m.group(1)))
+        elif m.group(1) in SYMBOL_TABLE:
+            return '0{:0=15b}\n'.format(SYMBOL_TABLE[m.group(1)])
+        else:
+            SYMBOL_TABLE[m.group(1)] = SYMBOL_TABLE['INS_PTR']
+            SYMBOL_TABLE['INS_PTR'] += 1
+            return '0{:0=15b}\n'.format(SYMBOL_TABLE[m.group(1)])
     else:
         return 'no match for A'
 
 def parseC(line):
     m = re.split(r'^\s*([MDA+\-=01&!|]*);?([JGELTMPNQ]{0,3})', line, maxsplit=3)
     if m:
-        dest, comp = m[1].split('=')
+        try:
+            dest, comp = m[1].split('=')
+        except (ValueError):
+            comp = m[1]
+            dest = ''
         jump = m[-2]
         return '111{comp}{dest}{jump}\n'.format(comp=COMP_TABLE[comp], dest=DEST_TABLE[dest], jump=JUMP_TABLE[jump])
     else:
         return 'no match for C'
 
+def analyzeSymbols(file):
+    line_count = -1
+    for line in file:
+        if ("@" in line or ";" in line or "=" in line) and (line[:2] != '//'):
+            line_count += 1
+        else:
+            m = re.match(r'^\s*\((.+)\)', line)
+            if m:
+                SYMBOL_TABLE[m.group(1)] = line_count + 1
+    file.seek(0)
+
 
 def parseLine(line):
-    if line[0] == '@':
+    if ("@" in line) and (line[:2] != '//'):
         return parseA(line)
-    elif line[0] in ['M', 'D', 'A'] or line[0].isnumeric():
+    elif (";" in line or "=" in line) and (line[:2] != '//'):
         return parseC(line)
     else:
          return ''
 
 def parse(file):
     parsed_data = ''
+    analyzeSymbols(file)
     for line in file:
         parsed_data += parseLine(line)
     writeFile(parsed_data, os.path.split(file.name)[1])
